3f0b72d054bc48e41120557d8be06adbb232c2f2
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-08-25 18:13:30 -0400

    primer trabajo

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 # Create your views here.
 def inicio(request):
-    contexto = {"nombre": "Hola Mundo Az"}
-    productos = [
-        {"nombre": "sensor 1", "valor": 100}
-        {"nombre": "sensor 2", "valor": 200}
-        {"nombre": "sensor 3", "valor": 1300}
+    Titulo = {"nombre": "Eco energy consumo Status"}
+    Dispositivos = [
+        {"nombre": "sensor 1", "consumo": 60},
+        {"nombre": "maquina 2", "consumo": 200},
+        {"nombre": "maquina prueba4", "consumo": 100}
     ]
 
-
+    max_consumo = 100
 
 
     return render(request, "dispositivos/inicio.html",{
-        "contexto": contexto,
-        "productos": productos
+        "Titulo": Titulo,
+        "Dispositivos": Dispositivos,
+        "max_consumo" : max_consumo
         })
