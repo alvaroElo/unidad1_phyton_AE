2fc3e47feb5377b1867f97fe6c8f95ffbe3da0bc
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-09-01 17:53:44 -0400

    create admin

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Zona, Dispositivo
+
+admin.site.register([Categoria, Zona])
+
+@admin.register(Dispositivo)
+class DispositivoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria', 'zona', 'consumo_maximo')
+    list_filter = ("estado", "categoria")  # Filtros en el lateral
+    search_fields = ("nombre",)             # Buscador por NOMBRE
--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+# ----------------------------
+# Modelo Base con atributos comunes
+# ----------------------------
+class BaseModel(models.Model):
+    ESTADOS = [
+        ("ACTIVO", "Activo"),
+        ("INACTIVO", "Inactivo"),
+    ]
+
+    estado = models.CharField(max_length=10, choices=ESTADOS, default="ACTIVO")
+    created_at = models.DateTimeField(auto_now_add=True)  # se asigna al crear
+    updated_at = models.DateTimeField(auto_now=True)      # se actualiza cada vez que se guarda
+    deleted_at = models.DateTimeField(null=True, blank=True)  # opcional para borrado lógico
+
+    class Meta:
+        abstract = True  # no crea tabla, solo se hereda
+
+
+# ----------------------------
+# Tablas principales
+# ----------------------------
+class Categoria(BaseModel):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Zona(BaseModel):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Dispositivo(BaseModel):
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    zona = models.ForeignKey(Zona, on_delete=models.CASCADE)
+    consumo_maximo = models.IntegerField()  # watts
+
+    def __str__(self):
+        return self.nombre
