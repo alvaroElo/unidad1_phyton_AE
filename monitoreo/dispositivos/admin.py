5a8d55eddf95d2f77b92ce735383ccec0500d93b
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-09-01 18:13:46 -0400

    models profe

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
+from .models import Categoria, Zona, Dispositivo, Medicion
 
-# Register your models here.
-from .models import Categoria, Zona, Dispositivo
+# Registro básico (todas las tablas aparecen en el panel)
+admin.site.register([Categoria, Zona, Medicion])
 
-admin.site.register([Categoria, Zona])
-
+# Registro avanzado para Dispositivo
 @admin.register(Dispositivo)
 class DispositivoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'categoria', 'zona', 'consumo_maximo')
+    list_display = ("nombre", "consumo_maximo", "estado", "categoria")
     list_filter = ("estado", "categoria")  # Filtros en el lateral
-    search_fields = ("nombre",)             # Buscador por NOMBRE
+    search_fields = ("nombre",)             # Buscador por nombre
